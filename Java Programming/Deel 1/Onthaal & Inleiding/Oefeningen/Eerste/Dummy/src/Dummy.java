653b1dde5dcfead8e644c32b7ad55068593e2171
Steven Excelmans <steven.excelmans@telenet.be>
2017-09-17 18:44:16 +0200

    [JAVA] Inleiding & Onthaal

---
+++ Java Programming/Deel 1/Onthaal & Inleiding/Oefeningen/Eerste/Dummy/src/Dummy.java
@@ -0,0 +1,9 @@
+/**
+ * @author Steven Excelmans
+ * @version 1.0 17/09/2017 18:32
+ */
+public class Dummy {
+    public static void main(String[] args) {
+        System.out.println("Hello world again!");
+    }
+}
